c828ba366d264dd7437fab0176fbf6a2d7e79376
deskawps23 <deskawiyatapraja@gmail.com>
2026-07-31 21:39:21 +0700

    Initialize App component with Firebase message

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text } from "react-native";
+import { app } from "./firebaseConfig";
+
+export default function App() {
+  return <Text>Firebase Zeevanna sudah aktif 🚀</Text>;
+}
